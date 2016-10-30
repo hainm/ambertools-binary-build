b08f9f600d1bc4037e6fec23b6c735f0c25f8067
Hai Nguyen <hainm.comp@gmail.com>
2016-10-30 13:16:26 -0400

    bash syntax

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 source scripts/download_AT16.sh
 tar -xf AmberTools16.tar.bz2 && cp -rf recipe-prebuild amber16/
 osname=`uname`
-if [ $osname = 'Linux']; then
+if [ $osname = 'Linux' ]; then
     source run_docker_build.sh
 else
     source run_oxs_build.sh
