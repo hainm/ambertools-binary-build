a0e9b574d1cb0e318f5e120c0fef0217b2f4ced5
Hai Nguyen <hainm.comp@gmail.com>
2016-10-30 13:26:18 -0400

    typos

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 
 source scripts/download_AT16.sh
 tar -xf AmberTools16.tar.bz2 && cp -rf recipe-prebuild amber16/
-osname=`uname`
-if [ $osname = 'Linux' ]; then
+osname=`python -c "import sys; print(sys.platform)"`
+if [ $osname = 'linux' ]; then
     source run_docker_build.sh
 else
     source run_oxs_build.sh
