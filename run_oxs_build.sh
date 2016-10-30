a12f88399dcae3f74af3e1e833eeaa7aab8286ce
Hai Nguyen <hainm.comp@gmail.com>
2016-10-30 14:05:49 -0400

    try brew

--- run_oxs_build.sh
+++ run_oxs_build.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+brew tap homebrew/science;
+brew update;
+brew install netcdf fftw;
 BZ2FILE=$HOME/miniconda/conda-bld/*/ambertools-16-1.tar.bz2
-
 wget http://repo.continuum.io/miniconda/Miniconda-3.7.0-MacOSX-x86_64.sh -O miniconda.sh
 bash miniconda.sh -b
 export PATH=$HOME/miniconda/bin:\$PATH
