f6180f5197eac520847426ebb4c2e87ebaa9d37f
Hai Nguyen <hainm.comp@gmail.com>
2016-10-30 13:15:47 -0400

    else

--- setup.sh
+++ setup.sh
@@ -5,6 +5,6 @@ tar -xf AmberTools16.tar.bz2 && cp -rf recipe-prebuild amber16/
 osname=`uname`
 if [ $osname = 'Linux']; then
     source run_docker_build.sh
-lse
+else
     source run_oxs_build.sh
 fi
