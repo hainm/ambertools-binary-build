7cb021c5facae8c6e603e49d98a5a003d85d0115
Hai Nguyen <hainm.comp@gmail.com>
2016-12-03 11:20:54 -0500

    where

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -7,7 +7,7 @@ copy %RECIPE_DIR%\configure2 %AMBERHOME%\AmberTools\src\
 python update_amber --show-applied-patches
 python update_amber --update
 python update_amber --show-applied-patches
-echo n | bash .\configure -noX11 --with-python `which python` gnu
+echo n | bash .\configure -noX11 --with-python C:\Miniconda3\python.exe gnu
 mingw32-make
 mingw32-make install
 xcopy /E %PREFIX%\bin %PREFIX%\Scripts\
