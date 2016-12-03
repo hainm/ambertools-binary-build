96ac3a58d063bcfca3c026f8cfa7457a3abb913a
Hai Nguyen <hainm.comp@gmail.com>
2016-12-03 11:29:20 -0500

    try

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,13 +1,10 @@
-set CC=gcc.exe
-set CXX=g++.exe 
-set FC=gfortran.exe
-
 set AMBERHOME=%cd%
 copy %RECIPE_DIR%\configure2 %AMBERHOME%\AmberTools\src\
 python update_amber --show-applied-patches
 python update_amber --update
 python update_amber --show-applied-patches
 echo n | bash .\configure -noX11 --with-python C:\Miniconda3\python.exe gnu
+make
 mingw32-make
 mingw32-make install
 xcopy /E %PREFIX%\bin %PREFIX%\Scripts\
