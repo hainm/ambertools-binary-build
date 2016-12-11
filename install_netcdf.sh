52274fd44aba6b64989c17eaa1285f8ef75cae93
Hai Nguyen <hainm.comp@gmail.com>
2016-12-11 04:03:40 -0500

    install fortran netcdf

--- install_netcdf.sh
+++ install_netcdf.sh
@@ -1,8 +1,18 @@
-cd $AMBERHOME
-mkdir lib
-mkdir include
-mkdir bin
+# c
 cd $AMBERHOME/AmberTools/src/netcdf-4.3.0
 sh configure --enable-static --disable-netcdf-4 --prefix=/usr/local --disable-dap --host x86_64-w64-mingw32
+make install
+
+# fortran
+cd $AMBERHOME/AmberTools/netcdf-fortran-4.2
+# The NetCDF Fortran config script will need access to the C library.
+export LD_LIBRARY_PATH="$AMBERHOME/lib:$LD_LIBRARY_PATH"
+echo "\tConfiguring NetCDF Fortran interface (may be time-consuming)...\n"
+make clean
+sh configure \
+              LDFLAGS="-L$AMBERHOME/lib" \
+              CPPFLAGS="-I$AMBERHOME/include" \
+              --enable-shared \
+              --prefix=/usr/loca/
 make install
 cd $AMBERHOME
