d3b597cd900202a3e8ec07f94dd614e3dc9df1f3
Hai Nguyen <hainm.comp@gmail.com>
2016-12-11 04:26:41 -0500

    more?

--- install_netcdf.sh
+++ install_netcdf.sh
@@ -8,14 +8,10 @@ cd $AMBERHOME/AmberTools/src/netcdf-fortran-4.2
 # The NetCDF Fortran config script will need access to the C library.
 export LD_LIBRARY_PATH="$AMBERHOME/lib:$LD_LIBRARY_PATH"
 echo "\tConfiguring NetCDF Fortran interface (may be time-consuming)...\n"
-make clean
 NFDIR=/usr/local/
-CPPFLAGS=-I${NCDIR}/include
-LDFLAGS=-L${NCDIR}/lib
-sh configure \
-              LDFLAGS=${LDFLAGS} \
-              CPPFLAGS=${CPPFLAGS} \
-              --disable-shared \
-              --prefix=/usr/local/
+export CPPFLAGS=-I${NCDIR}/include
+export LDFLAGS=-L${NCDIR}/lib
+echo $CPPFLAGS $LDFLAGS
+sh configure --disable-shared --prefix=/usr/local
 make install
 cd $AMBERHOME
