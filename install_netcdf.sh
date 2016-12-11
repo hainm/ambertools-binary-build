87bd06ba3d3ebaeebb7b9dee502e53093d57b17f
Hai Nguyen <hainm.comp@gmail.com>
2016-12-11 13:46:06 -0500

    dummy try

--- install_netcdf.sh
+++ install_netcdf.sh
@@ -2,6 +2,7 @@
 cd $AMBERHOME/AmberTools/src/netcdf-4.3.0
 sh configure --enable-static --disable-netcdf-4 --prefix=/usr/local --disable-dap --host x86_64-w64-mingw32
 make install
+make check
 
 # fortran
 cd $AMBERHOME/AmberTools/src/netcdf-fortran-4.2
@@ -11,6 +12,8 @@ echo "\tConfiguring NetCDF Fortran interface (may be time-consuming)...\n"
 export CPPFLAGS=-I/usr/local/include
 export LDFLAGS=-L/usr/local/lib
 echo $CPPFLAGS $LDFLAGS
-sh configure --disable-shared --prefix=/usr/local
+sh configure --enable-static --disable-netcdf-4 --prefix=/usr/local --disable-dap --host x86_64-w64-mingw32
 make install
+make check
+
 cd $AMBERHOME
