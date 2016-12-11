fa4dcd095b54b3e732924dad363c47ac246067a6
Hai Nguyen <hainm.comp@gmail.com>
2016-12-11 03:19:19 -0500

    make?

--- install_netcdf.sh
+++ install_netcdf.sh
@@ -1,3 +1,7 @@
+cd $AMBERHOME
+mkdir lib
+mkdir include
+mkdir bin
 cd $AMBERHOME/AmberTools/src/netcdf-4.3.0
 sh configure --enable-static --disable-netcdf-4 --prefix=$AMBERHOME --disable-dap --host x86_64-w64-mingw32
 make install
